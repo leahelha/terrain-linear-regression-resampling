71a480b8c7dfd98e0cda5113786a423c928bd8e9
Vetle Henrik Hvoslef <vetlehh@uio.no>
2023-10-05 14:37:15 +0200

    Rough plotting for beta verdiene for OLS

--- reg_class.py
+++ reg_class.py
@@ -32,54 +32,20 @@ class regression_class:
         
         self.make_design_matrix()
         self.scale_design_matrix()
-    
-    def ols_regression(self):
-        '''Calculates OLS for polynomials of degree 1 to n_deg_max.'''
-        for i in range(self.n_deg_max):
-            ols_results = self.fit_predict_ols(i+1)
-            self.ols["beta"][i] = ols_results[0]
-            self.ols["mse_train"][i] = ols_results[1]
-            self.ols["mse_test"][i] = ols_results[2]
-            self.ols["r2_train"][i] = ols_results[3]
-            self.ols["r2_test"][i] = ols_results[4]
 
-    def ridge_regression(self):
-        '''Calculates Ridge regression for polynomials of degree 1 to n_deg_max.'''
-        for i in range(self.n_deg_max):
-            ridge_results = self.fit_predict_ridge(i+1)
-            self.ridge["beta"][i] = ridge_results[0]
-            self.ridge["mse_train"][i] = ridge_results[1]
-            self.ridge["mse_test"][i] = ridge_results[2]
-            self.ridge["r2_train"][i] = ridge_results[3]
-            self.ridge["r2_test"][i] = ridge_results[4]
-    
-    def lasso_regression(self):
-        '''Calculates Lasso regression for polynomials of degree 1 to n_deg_max.'''
-        for i in range(self.n_deg_max):
-            lasso_results = self.fit_predict_lasso(i+1)
-            self.lasso["beta"][i] = lasso_results[0]
-            self.lasso["mse_train"][i] = lasso_results[1]
-            self.lasso["mse_test"][i] = lasso_results[2]
-            self.lasso["r2_train"][i] = lasso_results[3]
-            self.lasso["r2_test"][i] = lasso_results[4]
+    def make_design_matrix(self):
+        '''Makes design matrix and splits into training and test data'''
+        self.X = PolynomialFeatures(self.n_deg_max, include_bias = False).fit_transform(self.x) # without intercept
+        # self.X = PolynomialFeatures(self.n_deg_max, include_bias = True).fit_transform(self.x) # without intercept
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X, self.y, test_size = 0.2, random_state = 3) # random_state gives same partition across multiple function calls
 
-    def plot_ols_results(self):
-        '''Plots MSE, R2 score and beta values for OLS regression and saves to file.'''
-        self.plot_ols(self.ols["mse_train"], self.ols["mse_test"], "Mean Squared Error", "mse_ols")
-        self.plot_ols(self.ols["r2_train"], self.ols["r2_test"], f"$R^2$", "r2_ols")
-        # self.plot_beta_ols(self.ols["beta"], "beta_ols")
-    
-    def plot_ridge_results(self):
-        '''Plots MSE, R2 score and beta values for Ridge regression and saves to file.'''
-        self.plot_ridge_or_lasso(self.ridge["mse_train"], self.ridge["mse_test"], "Mean Squared Error", "mse_ridge")
-        self.plot_ridge_or_lasso(self.ridge["r2_train"], self.ridge["r2_test"], f"$R^2$", "r2_ridge")
-        # self.plot_beta_ridge_or_lasso(self.ridge["beta"], self.lmbda, "beta_ridge")
-
-    def plot_lasso_results(self):
-        '''Plots MSE, R2 score and beta values for Lasso regression and saves to file.'''
-        self.plot_ridge_or_lasso(self.lasso["mse_train"], self.lasso["mse_test"], "Mean Squared Error", "mse_lasso")
-        self.plot_ridge_or_lasso(self.lasso["r2_train"], self.lasso["r2_test"], f"$R^2$", "r2_lasso")
-        # self.plot_beta_ridge_or_lasso(self.lasso["beta"], self.lmbda, "beta_lasso")
+    def scale_design_matrix(self):
+        '''Scales data by subtracting mean'''
+        self.X_scaler = np.mean(self.X_train, axis = 0)
+        self.y_scaler = np.mean(self.y_train)
+        self.X_train_scaled = self.X_train - self.X_scaler
+        self.y_train_scaled = self.y_train - self.y_scaler
+        self.X_test_scaled = self.X_test - self.X_scaler
 
     def predict_ols(self, pol_degree):
         '''Makes a prediction with OLS model of polynomial degree pol_deg, using all X data'''
@@ -108,52 +74,27 @@ class regression_class:
         prediction = X @ self.lasso["beta"][pol_degree-1][lmbda_n] + self.y_scaler
         return prediction
 
-    def plot_ols(self, train_results, test_results, ylabel, name):
-        '''Plots either MSE or R2 score for train and test data from OLS and saves to file.'''
-        plt.figure(figsize = (6,4))
-        plt.plot(range(1, len(train_results)+1), train_results, label = "Training data")
-        plt.plot(range(1, len(train_results)+1), test_results, label = "Test data")
-        plt.xlabel("Polynomial degree")
-        plt.ylabel(ylabel)
-        plt.legend()
-        plt.savefig(f"plots/{name}.pdf")
-    
-    def plot_ridge_or_lasso(self, train_results, test_results, ylabel, name):
-        '''Plots either MSE or R2 score for train and test data from Ridge or Lasso regression and saves to file.'''
-        plt.figure(figsize = (6,12))
-        for i in range(self.n_deg_max): # one subplot for each polynomial degree
-            plt.subplot(self.n_deg_max, 1, i+1)
+    def beta_ols(self, X, y):
+        '''Given the design matrix X and the output y, calculates the coefficients beta using OLS.'''
+        beta = np.linalg.inv(X.T @ X) @ X.T @ y
+        return beta
 
-            plt.semilogx(self.lmbda, train_results[i], label = "Training data")
-            plt.semilogx(self.lmbda, test_results[i], label = "Test data")
+    def beta_ridge(self, X, y, lmbda):
+        '''Given the design matrix X, the output y and the parameter lmbda, calculates the coefficients beta using OLS.'''
+        n = np.shape(X)[1]
+        beta = np.linalg.inv(X.T @ X + lmbda*np.eye(n)) @ X.T @ y
+        return beta
 
-            plt.title(f"Polynomial of order {i+1}")
-            plt.xlabel("$\lambda$")
-            plt.ylabel(ylabel)
-            plt.legend()
-        plt.tight_layout()
-        plt.savefig(f"plots/{name}.pdf")
+    def mse_own(self, y_tilde, y):
+        '''Calculates the mean square error of a prediction y_tilde.'''
+        mse = 1/len(y) * np.sum((y-y_tilde)**2)
+        return mse
 
-    def plot_beta_ols(self, beta, name):
-        '''Plots beta values with standard deviation from OLS regression.'''
-        plt.figure(figsize = (15,5))
-        labels = ["$x$", "$y$", "$x^2$", "$xy$", "$y^2$", "$x^3$", "$x^2y$", "$xy^2$", "$y^3$", "$x^4$", "$x^3y$",
-                "$x^2y^2$", "$xy^3$", "$y^4$", "$x^5$", "$x^4y$", "$x^3y^2$", "$x^2y^3$", "$xy^4$", "$y^5$"]
-        for i in range(len(beta)):
-            plt.subplot(1, len(beta), i+1)
-            plt.bar(labels[:len(beta[i])], beta[i])
-        plt.savefig(f"plots/{name}.pdf")
-
-    def plot_beta_ridge_or_lasso(self, beta, lmbda, name):
-        '''Plots beta values with standard deviation from Ridge or Lasso regression.'''
-        labels = ["$x$", "$y$", "$x^2$", "$xy$", "$y^2$", "$x^3$", "$x^2y$", "$xy^2$", "$y^3$", "$x^4$", "$x^3y$",
-                "$x^2y^2$", "$xy^3$", "$y^4$", "$x^5$", "$x^4y$", "$x^3y^2$", "$x^2y^3$", "$xy^4$", "$y^5$"]
-        for i in range(len(lmbda)):
-            plt.figure(figsize = (15,5))
-            for j in range(len(beta)):
-                plt.subplot(1, len(beta), j+1)
-                plt.bar(labels[:len(beta[j][i])], beta[j][i])
-            plt.savefig(f"plots/{name}_{lmbda[i]}.pdf")
+    def r2_own(self, y_tilde, y):
+        '''Calculates the R^2 score of a prediction y_tilde.'''
+        a = np.sum((y-y_tilde)**2)
+        b = np.sum((y-np.mean(y))**2)
+        return 1 - a/b
 
     def fit_predict_ols(self, pol_degree):
         '''For a given polynomial order, makes and trains an OLS model and calculates MSE for both training and test data.'''
@@ -237,48 +178,43 @@ class regression_class:
 
         return beta, mse_train, mse_test, r2_train, r2_test
 
-    def beta_ols(self, X, y):
-        '''Given the design matrix X and the output y, calculates the coefficients beta using OLS.'''
-        beta = np.linalg.inv(X.T @ X) @ X.T @ y
-        return beta
+    
+    def ols_regression(self):
+        '''Calculates OLS for polynomials of degree 1 to n_deg_max.'''
+        for i in range(self.n_deg_max):
+            ols_results = self.fit_predict_ols(i+1)
+            self.ols["beta"][i] = ols_results[0]
+            self.ols["mse_train"][i] = ols_results[1]
+            self.ols["mse_test"][i] = ols_results[2]
+            self.ols["r2_train"][i] = ols_results[3]
+            self.ols["r2_test"][i] = ols_results[4]
 
-    def beta_ridge(self, X, y, lmbda):
-        '''Given the design matrix X, the output y and the parameter lmbda, calculates the coefficients beta using OLS.'''
-        n = np.shape(X)[1]
-        beta = np.linalg.inv(X.T @ X + lmbda*np.eye(n)) @ X.T @ y
-        return beta
-
-    def mse_own(self, y_tilde, y):
-        '''Calculates the mean square error of a prediction y_tilde.'''
-        mse = 1/len(y) * np.sum((y-y_tilde)**2)
-        return mse
-
-    def r2_own(self, y_tilde, y):
-        '''Calculates the R^2 score of a prediction y_tilde.'''
-        a = np.sum((y-y_tilde)**2)
-        b = np.sum((y-np.mean(y))**2)
-        return 1 - a/b
-
-    def make_design_matrix(self):
-        '''Makes design matrix and splits into training and test data'''
-        self.X = PolynomialFeatures(self.n_deg_max, include_bias = False).fit_transform(self.x) # without intercept
-        # self.X = PolynomialFeatures(self.n_deg_max, include_bias = True).fit_transform(self.x) # without intercept
-        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X, self.y, test_size = 0.2, random_state = 3) # random_state gives same partition across multiple function calls
-
-    def scale_design_matrix(self):
-        '''Scales data by subtracting mean'''
-        self.X_scaler = np.mean(self.X_train, axis = 0)
-        self.y_scaler = np.mean(self.y_train)
-        self.X_train_scaled = self.X_train - self.X_scaler
-        self.y_train_scaled = self.y_train - self.y_scaler
-        self.X_test_scaled = self.X_test - self.X_scaler
+    def ridge_regression(self):
+        '''Calculates Ridge regression for polynomials of degree 1 to n_deg_max.'''
+        for i in range(self.n_deg_max):
+            ridge_results = self.fit_predict_ridge(i+1)
+            self.ridge["beta"][i] = ridge_results[0]
+            self.ridge["mse_train"][i] = ridge_results[1]
+            self.ridge["mse_test"][i] = ridge_results[2]
+            self.ridge["r2_train"][i] = ridge_results[3]
+            self.ridge["r2_test"][i] = ridge_results[4]
+    
+    def lasso_regression(self):
+        '''Calculates Lasso regression for polynomials of degree 1 to n_deg_max.'''
+        for i in range(self.n_deg_max):
+            lasso_results = self.fit_predict_lasso(i+1)
+            self.lasso["beta"][i] = lasso_results[0]
+            self.lasso["mse_train"][i] = lasso_results[1]
+            self.lasso["mse_test"][i] = lasso_results[2]
+            self.lasso["r2_train"][i] = lasso_results[3]
+            self.lasso["r2_test"][i] = lasso_results[4]
     
     def find_optimal_lambda(self, type):
         '''For either Ridge or Lasso regression, finds and returns lambda value that gives lowest MSE_test and corresponding MSE_test for each polynomial degree.'''
         
-        if (type == "ridge"):
+        if type == "ridge":
             mse_values = self.ridge["mse_test"]
-        elif (type == "lasso"):
+        elif type == "lasso":
             mse_values = self.lasso["mse_test"]
         else:
             print("Must specify 'ridge' or 'lasso' when calling optimal_lambda.")
@@ -296,8 +232,81 @@ class regression_class:
                     min_el = mse_values[i][j]
                     min_index = j
             optimaL_values[i] = (self.lmbda[min_index], min_el)
-        
         return optimaL_values
+
+
+    def plot_ols(self, train_results, test_results, ylabel, name):
+        '''Plots either MSE or R2 score for train and test data from OLS and saves to file.'''
+        plt.figure(figsize = (6,4))
+        plt.plot(range(1, len(train_results)+1), train_results, label = "Training data")
+        plt.plot(range(1, len(train_results)+1), test_results, label = "Test data")
+        plt.xlabel("Polynomial degree")
+        plt.ylabel(ylabel)
+        plt.legend()
+        plt.savefig(f"plots/{name}.pdf")
+
+    
+    def plot_ridge_or_lasso(self, train_results, test_results, ylabel, name):
+        '''Plots either MSE or R2 score for train and test data from Ridge or Lasso regression and saves to file.'''
+        plt.figure(figsize = (6,12))
+        for i in range(self.n_deg_max): # one subplot for each polynomial degree
+            plt.subplot(self.n_deg_max, 1, i+1)
+
+            plt.semilogx(self.lmbda, train_results[i], label = "Training data")
+            plt.semilogx(self.lmbda, test_results[i], label = "Test data")
+
+            plt.title(f"Polynomial of order {i+1}")
+            plt.xlabel("$\lambda$")
+            plt.ylabel(ylabel)
+            plt.legend()
+        plt.tight_layout()
+        plt.savefig(f"plots/{name}.pdf")
+
+    def plot_beta_ols(self, beta, name, degrees = None):
+        '''Plots beta values with standard deviation from OLS regression.'''
+        if degrees is None:
+            n_deg_max = self.n_deg_max
+            degrees = range(1, n_deg_max + 1, 2)
+
+        plt.figure(figsize = (15,5))
+        for degree in degrees:
+            indicies = range(len(beta[degree - 1]))
+            plt.bar(indicies, beta[degree - 1], label = f"degree = {degree}")
+        plt.legend()
+        plt.show()
+        # plt.savefig(f"plots/{name}.pdf")
+
+    def plot_beta_ridge_or_lasso(self, beta, lmbda, name):
+        '''Plots beta values with standard deviation from Ridge or Lasso regression.'''
+        labels = ["$x$", "$y$", "$x^2$", "$xy$", "$y^2$", "$x^3$", "$x^2y$", "$xy^2$", "$y^3$", "$x^4$", "$x^3y$",
+                "$x^2y^2$", "$xy^3$", "$y^4$", "$x^5$", "$x^4y$", "$x^3y^2$", "$x^2y^3$", "$xy^4$", "$y^5$"]
+        for i in range(len(lmbda)):
+            plt.figure(figsize = (15,5))
+            for j in range(len(beta)):
+                plt.subplot(1, len(beta), j+1)
+                plt.bar(labels[:len(beta[j][i])], beta[j][i])
+            plt.savefig(f"plots/{name}_{lmbda[i]}.pdf")
+                #     for j in range(len(degree)):
+                # plt.bar(, beta[i], labels = "degree = {degree}")
+
+    def plot_ols_results(self, name = "beta_ols"):
+        '''Plots MSE, R2 score and beta values for OLS regression and saves to file.'''
+        # self.plot_ols(self.ols["mse_train"], self.ols["mse_test"], "Mean Squared Error", "mse_ols")
+        # self.plot_ols(self.ols["r2_train"], self.ols["r2_test"], f"$R^2$", "r2_ols")
+        self.plot_beta_ols(self.ols["beta"], name)
+    
+    def plot_ridge_results(self):
+        '''Plots MSE, R2 score and beta values for Ridge regression and saves to file.'''
+        self.plot_ridge_or_lasso(self.ridge["mse_train"], self.ridge["mse_test"], "Mean Squared Error", "mse_ridge")
+        self.plot_ridge_or_lasso(self.ridge["r2_train"], self.ridge["r2_test"], f"$R^2$", "r2_ridge")
+        self.plot_beta_ridge_or_lasso(self.ridge["beta"], self.lmbda, "beta_ridge")
+
+    def plot_lasso_results(self):
+        '''Plots MSE, R2 score and beta values for Lasso regression and saves to file.'''
+        self.plot_ridge_or_lasso(self.lasso["mse_train"], self.lasso["mse_test"], "Mean Squared Error", "mse_lasso")
+        self.plot_ridge_or_lasso(self.lasso["r2_train"], self.lasso["r2_test"], f"$R^2$", "r2_lasso")
+        self.plot_beta_ridge_or_lasso(self.lasso["beta"], self.lmbda, "beta_lasso")
+
 
 def FrankeFunction(x,y):
     '''Calculates the two-dimensional Franke's function.'''
@@ -312,7 +321,7 @@ def add_noise(data, std):
     noise_matrix = np.random.normal(0, std, data.shape)
     return data + noise_matrix
 
-if (__name__ == "__main__"):
+def main():
     # Set up dataset
     n = 11 # number of points along one axis, total number of points will be n^2
     rng = np.random.default_rng(seed = 25) # seed to ensure same numbers over multiple runs
@@ -335,10 +344,13 @@ if (__name__ == "__main__"):
 
     # Do regression
     model.ols_regression()
-    model.ridge_regression()
-    model.lasso_regression()
+    # model.ridge_regression()
+    # model.lasso_regression()
 
     # Plot results
-    model.plot_ols_results()
-    model.plot_ridge_results()
-    model.plot_lasso_results()
+    model.plot_ols_results(name = "testing")
+    # model.plot_ridge_results()
+    # model.plot_lasso_results()
+
+if __name__ == "__main__":
+    main()
